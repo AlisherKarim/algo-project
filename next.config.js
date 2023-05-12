c3d3a631abd2cb18ce5cad03abfc9ae8ad61df8b
Kaiyrly Mukhametkarim <kayrasktl@gmail.com>
2023-05-12 13:29:41 +0900

    next.config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const version = process.env.BUILD_VERSION
 const nextConfig = {
-  reactStrictMode: true,
-  output: "standalone",
-  assetPrefix: `https://next-app-lambda.s3.us-east-1.amazonaws.com/${version}`,
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true
 };
 module.exports = nextConfig;
